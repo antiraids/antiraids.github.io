510c4cdbb37ad657c03d1f6fe977487a23a95530
Sami Capulcu <setatimmi@gmail.com>
2019-12-06 16:12:55 +0000

    Added page template

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//Wow JS
+new WOW().init();
+
+//AOS JS
+AOS.init();
+
+
+
+// Slick Slider carousel
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+
+  $('.caption_slides').slick({
+    dots: true
+  });
+
+});
